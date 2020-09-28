b925423492695266fc800b27fb548dfce371f454
Halil Azyikmis <hazyikmis@yahoo.com>
2020-09-29 00:43:33 +0200

    more on decorators - 2

--- decorators2.ts
+++ decorators2.ts
@@ -5,16 +5,35 @@ class Bot {
   @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
+  /*
   //@logError
   @logError('Oops, bot was sunk in the ocean!')
   pilot(): void {
     throw new Error('');
     //console.log('swish');
   }
+*/
+
+  //pilot(@parameterDecorator speed: string): void {
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator wind: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('swiiiiish');
+    }
+  }
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
 }
 
 /*
@@ -49,10 +68,10 @@ function logError(errMessage: string) {
 //WE CANNOT READ THE PROPERTY OF THE INSTANCE OF A CLASS IN DECORATORS!!!
 function testDecorator(target: any, key: string) {
   //console.log(target);
-  //console.log(key);
-  console.log(target[key]); //undefined
-  console.log(target.color); //undefined
+  console.log(key);
+  console.log(target[key]); //undefined; if @testDecorator is before "color: string = 'red';" line
+  console.log(target.color); //undefined; if @testDecorator is before "color: string = 'red';" line
   //undefined because, "target" is prototype of the class, and prototypes in JS only stores methods, not attributes
 }
 
-new Bot().pilot();
+//new Bot().pilot();
